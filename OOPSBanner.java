d4b1609570934f947e2d3f64076c5d69be50300b
DEVANANDHA SAJITH <ds4877@srmist.edu.in>
2026-02-16 23:01:49 +0530

    UC2:Implemented modular method and HashMap

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,6 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBanner {
     public static void main(String[] args) {
-        // This is the simple use case: Printing to console
-        System.out.println("OOPS");
+        // UC2: Call the modular method instead of printing directly
+        printBanner("OOPS");
+    }
+
+    public static void printBanner(String text) {
+        // Using a Map to store character designs for reusability
+        Map<Character, String[]> alphabet = new HashMap<>();
+        
+        alphabet.put('O', new String[]{
+            "  *** ",
+            " * *",
+            " * *",
+            "  *** "
+        });
+
+        alphabet.put('P', new String[]{
+            " **** ",
+            " * *",
+            " **** ",
+            " * "
+        });
+
+        alphabet.put('S', new String[]{
+            "  **** ",
+            " * ",
+            "  *** ",
+            "     * ",
+            " **** "
+        });
+
+        System.out.println("Modular Banner System Initialized for: " + text);
     }
 }
